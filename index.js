57bf7a60dbbc1fc8f6b194389301006c60a7f317
Your NameAsadChowdhury020 <asadchowdhury020@gmail.com>
2025-12-09 21:41:53 +0600

    After adding donation requests in db

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
+      "http://localhost:5175",
       "https://b12-m11-session.web.app",
     ],
     credentials: true,
@@ -30,12 +31,12 @@ app.use(express.json());
 // jwt middlewares
 const verifyJWT = async (req, res, next) => {
   const token = req?.headers?.authorization?.split(" ")[1];
-  console.log(token);
+  // console.log(token);
   if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
   try {
     const decoded = await admin.auth().verifyIdToken(token);
     req.tokenEmail = decoded.email;
-    console.log(decoded);
+    // console.log(decoded);
     next();
   } catch (err) {
     console.log(err);
@@ -55,6 +56,7 @@ async function run() {
   try {
     const db = client.db("RedPulseDB");
     const usersCollection = db.collection("users");
+    const donationCollection = db.collection("donationRequests");
 
     // Store all the users in db
     app.post("/users", async (req, res) => {
@@ -127,6 +129,23 @@ async function run() {
         res.status(500).send({ message: "Failed to update user", error });
       }
     });
+
+    // Create a new donation request
+    app.post("/donation-requests", verifyJWT, async (req, res) => {
+      const donationData = req.body;
+
+      if (!donationData) {
+        return res.status(400).send({ message: "Invalid data" });
+      }
+
+
+        donationData.createdAt = new Date();
+
+        const result = await donationCollection.insertOne(donationData);
+
+        res.send(result);
+      }
+    );
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
